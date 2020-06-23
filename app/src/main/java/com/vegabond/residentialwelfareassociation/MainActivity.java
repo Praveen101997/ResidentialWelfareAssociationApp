f72b0810cefa2795901cac86b17855244de02406
Praveen Sharma <dce.pks@gmail.com>
2020-06-23 23:38:46 +0530

    Initial commit

---
+++ app/src/main/java/com/vegabond/residentialwelfareassociation/MainActivity.java
@@ -0,0 +1,40 @@
+package com.vegabond.residentialwelfareassociation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.vegabond.residentialwelfareassociation.registerandlogin.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogout;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.btnLogout);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
